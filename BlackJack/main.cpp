7102ba77f833227903b83c13d2c0bb024521e7ac
Alexdlr0 <alexdelarosalugo@gmail.com>
2025-05-26 23:54:53 -0600

    structdsssss

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
+
+struct Cartas{
+    //tipos de cartas y valor
+};
+
+struct Jugador{
+    //Nombre, puntos y juego
+};
+
+struct Casa{
+    //puntos y juego
+};
+
 int main( ){
     cout<<" --- BLACKJACK --- \n";
+    cout<<"Ingresa el nombre del jugador: ";
     return 0;
 }
