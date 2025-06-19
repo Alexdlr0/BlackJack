f94c2b26ae1d9836f349d941765aea0e048836e1
Alexdlr0 <alexdelarosalugo@gmail.com>
2025-06-19 17:43:17 -0600

    Sorteo de cartas y reparticion a medias jaja

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 enum Tipo{TREBOLES = 1, DIAMANTES, CORAZONES, PICAS};
 
+int cartasBarajeadas[52] = {0};
+int conteo = 0;
+
 struct DatosCartas{
     int tipo;
     char nombre[20];
@@ -19,10 +22,21 @@ struct Cartas{
 
 struct Jugador{
     //Nombre, puntos y juego
+    string nombre;
+    int puntos = 0;
 };
 
-struct Casa{
+struct Partida{
     //puntos y juego
+    int mazoJug[10] = {0};
+    int puntosJug = 0;
+    int mazoCasa[10] = {0};
+    int puntosCasa = 0;
+};
+
+struct Juego{
+    int cantidad;
+    Partida *partida;
 };
 
 string tipoCarta(int tipo){
@@ -40,9 +54,9 @@ string tipoCarta(int tipo){
             return "Picas";
             break;
         default:
+            return "Desconocido";
             break;
     }
-    return 0;
 }
 
 void crearBaraja(struct Cartas* cartas){
@@ -78,8 +92,62 @@ void crearBaraja(struct Cartas* cartas){
     }
 }
 
+void barajeo( ){
+    for (int i=0; i<52; i++){
+        cartasBarajeadas[i] = i;
+        cout<<cartasBarajeadas[i]<<endl;
+    }
+    cout<<endl;
+    srand(time(NULL));
+        for (int i = 51; i > 0; i--) {
+            int j = rand() % (i + 1);
+            swap(cartasBarajeadas[i], cartasBarajeadas[j]);
+        }
+    cout << "\nCartas barajeadas (índices):\n";
+        for (int i = 0; i < 52; i++) {
+            cout << cartasBarajeadas[i] <<endl;
+        }
+}
+
+void crearPartidas(struct Juego* juego, struct Cartas* cartas){
+    juego->partida = (struct Partida*) malloc(juego->cantidad * sizeof(struct Partida));
+}
+
+int darCarta(struct Cartas* cartas){
+    int valorAs=0;
+    if(cartas->datosCartas[cartasBarajeadas[conteo]].valor == 1){
+        do{
+            cout<<"Que valor eliges?, 1 o 11: ";
+            cin>>valorAs;
+        }while(valorAs != 1 && valorAs != 11);
+    }
+    return cartasBarajeadas[conteo++];
+}
+
+void rondasJuego(struct Juego* juego, struct Jugador* jugador, struct Cartas* cartas){
+    //Rondas de BlackJack
+    for(int i = 0; i<juego->cantidad; i++){
+        
+        //ronda i
+        //reparto 2 cartas cada uno
+        juego->partida[i].mazoJug[0] = darCarta(cartas);
+        juego->partida[i].mazoCasa[0] = darCarta(cartas);
+        juego->partida[i].mazoJug[1] = darCarta(cartas);
+        juego->partida[i].mazoCasa[1] = darCarta(cartas);
+        
+        cout<<"Carta jugador: \n"<< cartas->datosCartas[ juego->partida[i].mazoJug[0]].nombre <<endl
+                               <<cartas->datosCartas[ juego->partida[i].mazoJug[1]].nombre <<endl;
+        cout<<"Carta casa: \n"<< cartas->datosCartas[ juego->partida[i].mazoCasa[0]].nombre <<endl
+                            <<cartas->datosCartas[ juego->partida[i].mazoCasa[1]].nombre <<endl;
+        
+    }
+}
+
 int main( ){
     Cartas cartas;
+    Juego juego;
+    Jugador jugador;
+    
     crearBaraja(&cartas);
     
     cout<<" --- BLACKJACK --- \n";
@@ -88,5 +156,19 @@ int main( ){
         cout<<"Nombre: "<<cartas.datosCartas[i].nombre<<" Valor: "<<cartas.datosCartas[i].valor<<endl;
     }
     
+    cout<<"\nIngresa tu nombre: ";
+    getline(cin, jugador.nombre);
+    
+    do{
+        cout<<"\nCuantas rondas quieres jugar "<<jugador.nombre<<": ";
+        cin>>juego.cantidad;
+    } while(juego.cantidad<1);
+    
+    crearPartidas(&juego, &cartas);
+    barajeo();
+    rondasJuego(&juego, &jugador, &cartas);
+    
+    free(juego.partida);
+    free(cartas.datosCartas);
     return 0;
 }
